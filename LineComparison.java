68682de96802010ae2cd6c6ddf4e4fc39c38d0a1
aravindkarri <aravindkarri98495@gmail.com>
2021-06-22 10:00:07 +0530

    initial commit

---
+++ LineComparison.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.LineComparisonProblem;
+
+public class LineComparison {
+	
+	public static void main(String args[]) {
+	
+		System.out.println("Welcome to Line Comparison Computation");
+	}
+
+}
